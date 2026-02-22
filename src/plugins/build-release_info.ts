77be898e35e9a6df9f1cad6fdb36f090cd7245a5
albertosm27 <alberto3pt@gmail.com>
2026-02-22 19:59:38 +0100

    Add release from tag push

--- src/plugins/build-release_info.ts
+++ src/plugins/build-release_info.ts
@@ -13,14 +13,16 @@ export default function buildReleaseInfo(): PluginOption {
         writeBundle: () => {
             setImmediate(async () => {
                 const versionIdentifier = version.toString()
+                const [repositoryOwner, repositoryName] =
+                    process.env.GITHUB_REPOSITORY?.split('/') ?? ['crafter3d', 'mainsail']
                 const releaseInfoFile = await fs.promises.open(
                     path.resolve(__dirname, '../../dist/release_info.json'),
                     'w'
                 )
                 await releaseInfoFile.writeFile(
                     JSON.stringify({
-                        project_name: 'mainsail',
-                        project_owner: 'mainsail-crew',
+                        project_name: repositoryName,
+                        project_owner: repositoryOwner,
                         version: `v${versionIdentifier}`,
                     })
                 )
